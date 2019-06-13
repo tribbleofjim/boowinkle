2b2843398844bddca5f54508b2ac2d04bf0b327f
欲速则不达 <ruanweishui@yeah.net>
2019-06-13 12:46:07 +0800

    提交文件

---
+++ pages/info/info.js
@@ -0,0 +1,14 @@
+//logs.js
+let util = require('../../utils/util.js');
+let wechat = require("../../utils/wechat");
+let amap = require("../../utils/amap");
+Page({
+  data: {
+    steps: [],
+  },
+  onLoad() {
+    let steps = wx.getStorageSync("steps");
+    this.setData({ steps })
+    console.log(steps);
+  },
+});
